e5548643a1399d2b5cbcb7403312f44c38405e23
Seckin Ozturk <franseck102@gmail.com>
2025-01-10 21:32:23 -0500

    login style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,14 +3,15 @@ import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import image from "../assets/result2.svg"
+import logo from "../assets/Logo.png"
 import { Link } from "react-router-dom"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
-import { Button } from "@mui/material"
+import { Button, Card } from "@mui/material"
 
 const Login = () => {
     return (
-    <Container maxWidth="lg" sx={{bgcolor:"teal"}}>
+    <Container maxWidth="lg" style={{borderRadius:15}} sx={{bgcolor:"teal"}}>
       <Grid
         container
         justifyContent="center"
@@ -26,17 +27,17 @@ const Login = () => {
           </Typography>
         </Grid>
 
-        <Grid item xs={12} sm={10} md={6}>
-          <Avatar
-            sx={{
-              backgroundColor: "secondary.light",
-              m: "auto",
-              width: 40,
-              height: 40,
+        <Grid item xs={12} sm={10} md={6} style={{alignItems:"center", justifyContent:"center", textAlign:"center"}}>
+    
+          <img src={logo}
+            style={{
+ width:150,
+ borderRadius:15,
+
             }}
-          >
+   />
       
-          </Avatar>
+
           <Typography
             variant="h4"
             align="center"
@@ -75,7 +76,7 @@ const Login = () => {
         </Grid>
 
         <Grid item xs={10} sm={7} md={6}>
-          <Container sx={{width:400}}>
+          <Container sx={{width:450}}>
             <img  src={image} alt="img"  />
           </Container>
         </Grid>
